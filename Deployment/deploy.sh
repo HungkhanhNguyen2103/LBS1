61e9001e18ef0a97cf6017194aec6cf1002f30f3
Enzo Nguyen <enzo.brightsoft@gmail.com>
2025-03-16 18:18:56 +0700

    update deploy sh

--- Deployment/deploy.sh
+++ Deployment/deploy.sh
@@ -1,7 +1,7 @@
 ﻿# ireading_api
-docker build -t ireading_api -f Dockerfile.IREADINGAPI ..
+docker build -t ireading_api -f Dockerfile.IReadingAPI ..
 # ireading_web
-docker build -t ireading_web -f Dockerfile.IREADINGWEB ..
+docker build -t ireading_web -f Dockerfile.IReadingWeb ..
 
 docker-compose -f docker-compose.yml down
 docker-compose build
